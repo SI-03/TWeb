78bf2cbe7899fe5a6d4e5228eabdc84c32f742df
SI-03 <iuriesavciuc27@gmail.com>
2025-05-23 22:51:43 +0300

    al doilea commit

---
+++ WebApplication TW/Models/ToDoData.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication_TW.Entities.User;
+using WebApplication_TW.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_TW.Models
+{
+    public class TodoData
+    {
+        public string Subiect { get; set; }
+        public string Descriere { get; set; }
+        public string Lista { get; set; }
+        public DateTime TodoTime { get; set; }
+    }
+}
---
+++ WebApplication TW/Models/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_TW.Models
+{
+    public class UserLogin
+    {
+        public string Credential { get; set; }
+        public string Password { get; set; }
+        public DateTime LoginDateTime { get; set; }
+    }
+}
