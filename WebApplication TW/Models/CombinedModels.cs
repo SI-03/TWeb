d86adfe2e9b9ca2a5c335f5934063aabadf4dbe2
rascovschivictor <rascovschivictor1@gmail.com>
2025-05-24 16:51:24 +0300

    4. models combine

---
+++ WebApplication TW/Models/CombinedModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_TW.Models
+{
+    public class CombinedModels
+    {
+        public List<WebApplication_TW.Domain.Entities.User.TodoDbTable> TodoList { get; set; }
+        public WebApplication_TW.Models.TodoData TodoData { get; set; }  
+    }
+
+}
